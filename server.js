3107a12448a4e7ca6b45e1a8e646b0f77e24085a
hachimB <boubacarhachim@gmail.com>
2024-06-16 08:36:28 +0100

    Create a new user

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 const router = require('./routes/index');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', router);
 
