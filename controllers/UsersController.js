9761bdcfce07460405d4fd1dc826b21da6a66ef3
hachimB <boubacarhachim@gmail.com>
2024-06-16 07:36:07 +0100

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,20 +1,23 @@
-const sha1 = require('sha1');
+const crypto = require('crypto');
 const db = require('../utils/db');
 
 class UsersController {
   static postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      return res.status(400).send({ error: 'Missing email' });
     }
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      return res.status(400).send({ error: 'Missing password' });
     }
-    if (db.client.db(db.database).collection('users').findOne({ email })) {
-      return res.status(400).json({ error: 'Already exist' });
+    const existingUser = db.client.db(db.database).collection('users').findOne({ email });
+    if (existingUser) {
+      return res.status(400).send({ error: 'Already exist' });
     }
-    const hashedPassword = sha1(password);
-    return db.client.db(db.database).collection('users').insertOne({ email, password: hashedPassword });
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    const user = db.client.db(db.database).collection('users').insertOne({ email, password: hashedPassword }).save();
+    return res.status(201).send({ id: user.id, email: user.email });
   }
 }
 module.exports = UsersController;
