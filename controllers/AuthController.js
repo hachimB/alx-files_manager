ff8b2d249c164d822e13d64fb86a97fe9dff11a8
hachimB <boubacarhachim@gmail.com>
2024-06-17 12:22:15 +0100

    Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,47 +1,3 @@
-// const crypto = require('crypto');
-// const { v4: uuidv4 } = require('uuid');
-// const redisClient = require('../utils/redis');
-// const dbClient = require('../utils/db');
-
-// class AuthController {
-//   static async getConnect(req, res) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//       return res.status(401).send({ error: 'No credentials sent!' });
-//     }
-//     const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString();
-//     const [email, password] = auth.split(':');
-//     const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-//     const user = await dbClient.client.db(dbClient.database).collection('users').
-// findOne({ email, password: hashedPassword });
-//     if (!user) {
-//       return res.status(401).send({ error: 'Unauthorized' });
-//     }
-
-//     const token = uuidv4();
-//     const key = `auth_${token}`;
-//     await redisClient.set(key, user._id.toString(), 'EX', 24 * 60 * 60);
-//     return res.status(200).send({ token });
-//   }
-
-//   static async getDisconnect(req, res) {
-//     const token = req.headers['x-token'];
-//     if (!token) {
-//       return res.status(401).send({ error: 'Unauthorized' });
-//     }
-//     const key = `auth_${token}`;
-//     const user = await redisClient.get(key);
-//     if (!user) {
-//       return res.status(401).send({ error: 'Unauthorized' });
-//     }
-//     await redisClient.del(key);
-//     return res.status(204).send();
-//   }
-// }
-
-// module.exports = AuthController;
-
 const crypto = require('crypto');
 const { v4: uuidv4 } = require('uuid');
 const redisClient = require('../utils/redis');
@@ -57,23 +13,19 @@ class AuthController {
     const [email, password] = auth.split(':');
     const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
 
-    try {
-      const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ email, password: hashedPassword });
-      if (!user) {
-        return res.status(401).send({ error: 'Unauthorized' });
-      }
-
-      const token = uuidv4();
-      const key = `auth_${token}`;
-      await redisClient.set(key, user._id.toString(), 'EX', 24 * 60 * 60);
-      return res.status(200).send({ token });
-    } catch (err) {
-      return res.status(500).send({ error: 'Internal Server Error' });
+    const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ email, password: hashedPassword });
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
     }
+
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
+    return res.status(200).send({ token });
   }
 
   static async getDisconnect(req, res) {
-    const token = req.headers['x-token'];
+    const token = req.headers['X-Token'];
     if (!token) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,58 +1,3 @@
-// const crypto = require('crypto');
-// const dbClient = require('../utils/db');
-// const redisClient = require('../utils/redis');
-
-// class UsersController {
-//   static async postNew(req, res) {
-//     const { email, password } = req.body;
-
-//     if (!email) {
-//       return res.status(400).send({ error: 'Missing email' });
-//     }
-
-//     if (!password) {
-//       return res.status(400).send({ error: 'Missing password' });
-//     }
-
-//     try {
-//       const existingUser = await dbClient.client.db(dbClient.database).collection('users').
-// findOne({ email });
-
-//       if (existingUser) {
-//         return res.status(400).send({ error: 'Already exist' });
-//       }
-
-//       const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-//       const result = await dbClient.client.db(dbClient.database).collection('users').
-// insertOne({ email, password: hashedPassword });
-
-//       return res.status(201).send({ id: result.insertedId, email });
-//     } catch (err) {
-//       return res.status(500).send({ error: 'Internal Server Error' });
-//     }
-//   }
-
-//   static async getMe(req, res) {
-//     const token = req.headers['x-token'];
-//     if (!token) {
-//       return res.status(401).send({ error: 'Unauthorized' });
-//     }
-//     const key = `auth_${token}`;
-//     const userId = await redisClient.get(key);
-//     if (!userId) {
-//       return res.status(401).send({ error: 'Unauthorized' });
-//     }
-//     const user = await dbClient.client.db(dbClient.database).collection('users').
-// findOne({ _id: new dbClient.client.ObjectId(userId) });
-//     if (!user) {
-//       return res.status(404).send({ error: 'User not found' });
-//     }
-//     return res.status(200).send({ id: user._id, email: user.email });
-//   }
-// }
-// module.exports = UsersController;
-
 const crypto = require('crypto');
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
@@ -87,7 +32,7 @@ class UsersController {
   }
 
   static async getMe(req, res) {
-    const token = req.headers['x-token'];
+    const token = req.headers['X-Token'];
     if (!token) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
@@ -96,16 +41,11 @@ class UsersController {
     if (!userId) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
-    try {
-      const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ _id: new dbClient.client.ObjectId(userId) });
-      if (!user) {
-        return res.status(404).send({ error: 'User not found' });
-      }
-      return res.status(200).send({ id: user._id, email: user.email });
-    } catch (err) {
-      return res.status(500).send({ error: 'Internal Server Error' });
+    const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ _id: new dbClient.client.ObjectId(userId) });
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
     }
+    return res.status(200).send({ id: user._id, email: user.email });
   }
 }
-
 module.exports = UsersController;
