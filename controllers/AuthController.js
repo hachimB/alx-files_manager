1139126eba45b898dc75a91afbe445611b5cd405
hachimB <boubacarhachim@gmail.com>
2024-06-18 11:53:14 +0100

    Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -13,71 +13,29 @@ class AuthController {
     const [email, password] = auth.split(':');
     const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
 
-    try {
-      const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ email, password: hashedPassword });
-      if (!user) {
-        return res.status(401).send({ error: 'Unauthorized' });
-      }
-
-      const token = uuidv4();
-      const key = `auth_${token}`;
-      await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
-      return res.status(200).send({ token });
-    } catch (err) {
-      return res.status(500).send({ error: 'Internal Server Error' });
+    const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ email, password: hashedPassword });
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
     }
+
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
+    return res.status(200).send({ token });
   }
-  //   static async getConnect(req, res) {
-  //     const authHeader = req.headers.authorization;
-  //     if (!authHeader) {
-  //       return res.status(401).send({ error: 'No credentials sent!' });
-  //     }
-  //     const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString();
-  //     const [email, password] = auth.split(':');
-  //     const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
 
-  //     const user = await dbClient.client.db(dbClient.database).collection('users').
-  //   findOne({ email, password: hashedPassword });
-  //     if (!user) {
-  //       return res.status(401).send({ error: 'Unauthorized' });
-  //     }
-
-  //     const token = uuidv4();
-  //     const key = `auth_${token}`;
-  //     await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
-  //     return res.status(200).send({ token });
-  //   }
-
-  //   static async getDisconnect(req, res) {
-  //     const token = req.headers['x-token'];
-  //     if (!token) {
-  //       return res.status(401).send({ error: 'Unauthorized' });
-  //     }
-  //     const key = `auth_${token}`;
-  //     const user = await redisClient.get(key);
-  //     if (!user) {
-  //       return res.status(401).send({ error: 'Unauthorized' });
-  //     }
-  //     await redisClient.del(key);
-  //     return res.status(204).send();
-  //   }
   static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
     if (!token) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
-
     const key = `auth_${token}`;
-    try {
-      const user = await redisClient.get(key);
-      if (!user) {
-        return res.status(401).send({ error: 'Unauthorized' });
-      }
-      await redisClient.del(key);
-      return res.status(204).send();
-    } catch (err) {
-      return res.status(500).send({ error: 'Internal Server Error' });
+    const user = await redisClient.get(key);
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
     }
+    await redisClient.del(key);
+    return res.status(204).send();
   }
 }
 
